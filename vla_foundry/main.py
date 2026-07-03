0aa33ee9053e47a8d8a94487df12f2f3d1f0557f
xrdddd <douningjun@gmail.com>
2026-07-03 08:39:01 +0000

    updated according to jmercat's nice feedback.

--- vla_foundry/main.py
+++ vla_foundry/main.py
@@ -62,9 +62,6 @@ def main():
     # Parse config.
     cfg = draccus.parse(config_class=TrainExperimentParams)
     
-    # check config
-    check_cfg(cfg)
-    
     if cfg.resolve_configs:
         # Resolve configs for debugging. Program stops here if the flag is received.
         if is_master(cfg):
@@ -392,10 +389,6 @@ def main():
     if cfg.distributed.use_distributed and torch.distributed.is_initialized():
         torch.distributed.destroy_process_group()
 
-def check_cfg(cfg : TrainExperimentParams):
-    if cfg.model.type == "vlm":
-        assert cfg.data.img_num_tokens == (cfg.model.vit.img_size // cfg.model.vit.patch_size) ** 2, "parameter img_num_tokens is expected to be equal to the patches count."
-    # other like warning also can go here
     
 if __name__ == "__main__":
     main()
